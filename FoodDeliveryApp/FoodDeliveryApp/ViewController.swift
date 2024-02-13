e18c5a29717cb5b941e9282c0701d1f999ab5849
GLABBER <glabber@iMac-GLABBER.local>
2024-02-13 18:02:38 +0200

    Create base project, removed storyboard references

---
+++ FoodDeliveryApp/FoodDeliveryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodDeliveryApp
+//
+//  Created by GLABBER on 12.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
